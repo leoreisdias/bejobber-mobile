06dca1e3c0fc4bcb636ac7244b6ba10a7d938bec
leoreisdias <leoanrdord99@gmail.com>
2020-10-19 17:37:18 -0300

    trying app-bundle

--- App.tsx
+++ App.tsx
@@ -4,7 +4,6 @@ import { useFonts } from 'expo-font'
 import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito'
 import Routes from './src/routes';
 import { StatusBar } from 'react-native';
-import { AppLoading, Asset } from 'expo'
 
 export default function App() {
   const [isReady, setIsReady] = useState(false);
@@ -19,23 +18,6 @@ export default function App() {
     return null;
   }
 
-  const _cacheResourcesAsync = async () => {
-    const images = [require('./assets/snack-icon.png')];
-    const cacheImages = images.map(image => {
-      return Asset.fromModule(image).downloadAsync();
-    });
-    return Promise.all(cacheImages);
-  }
-
-  if (isReady === false) {
-    return (
-      <AppLoading
-        startAsync={_cacheResourcesAsync}
-        onFinish={() => setIsReady(true)}
-        onError={console.warn}
-      />
-    )
-  }
 
   return (<>
     <StatusBar hidden={true} />
